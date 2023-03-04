51e5a2b558d94ce412d99c2b3d96a6119f7e32c1
Ismailov Altynbek <altyni@gmail.com>
2023-03-04 18:46:41 +0600

    Added AaveV2 integration

--- contracts/facets/aaveV2/aaveProtocolV2.sol
+++ contracts/facets/aaveV2/aaveProtocolV2.sol
@@ -8,26 +8,26 @@ import { LibDiamond } from "../../libraries/LibDiamond.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@gnus.ai/contracts-upgradeable-diamond/metatx/MinimalForwarderUpgradeable.sol";
 import "@gnus.ai/contracts-upgradeable-diamond/metatx/ERC2771ContextUpgradeable.sol";
-import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 
-import "./IPool.sol";
+import "./ILendingPoolV2.sol";
 import "./IWrappedNativeToken.sol";
+import "./ILendingPoolAddressesProviderV2.sol";
 import "../handlerBase.sol";
 
-contract AaveV3Facet is ERC2771ContextUpgradeable, HandlerBase {
+contract AaveV2Facet is ERC2771ContextUpgradeable, HandlerBase {
     using SafeERC20 for IERC20;
     error COULD_NOT_PROCESS(string);
     
-    address public immutable PROVIDER;
     uint16 public constant REFERRAL_CODE = 0;
     address public immutable wrappedNativeToken;
+    address public immutable provider;
     
     constructor(MinimalForwarderUpgradeable forwarder, address _provider, address wrappedNativeToken_)
         ERC2771ContextUpgradeable(address(forwarder))
-        {PROVIDER = _provider;
-        wrappedNativeToken = wrappedNativeToken_;}
+        {provider = _provider;
+         wrappedNativeToken = wrappedNativeToken_;}
 
-    function executeAaveV3(
+    function executeAaveV2(
         uint24 _id
     ) external {
        address msgSender_ = _msgSender();   
@@ -39,35 +39,37 @@ contract AaveV3Facet is ERC2771ContextUpgradeable, HandlerBase {
 
         uint256 _amount = vt.voteProposalAttributes[_id].amount;
 
-        if (vt.voteProposalAttributes[_id].voteType == 220){
-        vt.voteProposalAttributes[_id].voteStatus =220;
+        //deposit 
+        if (vt.voteProposalAttributes[_id].voteType == 600){
+        vt.voteProposalAttributes[_id].voteStatus =600;
+       
         address addr = vt.voteProposalAttributes[_id].tokenID;
         _amount = _getBalance(addr, _amount);
-        _supply(addr, _amount);
+        _deposit(addr, _amount);
         }
 
-       else if (vt.voteProposalAttributes[_id].voteType == 221){
-        vt.voteProposalAttributes[_id].voteStatus =221;
+        //deposit ETH
+       else if (vt.voteProposalAttributes[_id].voteType == 601){
+        vt.voteProposalAttributes[_id].voteStatus =601;
         _amount = _getBalance(NATIVE_TOKEN_ADDRESS, _amount);
-
         IWrappedNativeToken(wrappedNativeToken).deposit{value: _amount}();        
-         _supply(wrappedNativeToken, _amount);
-
+         _deposit(wrappedNativeToken, _amount);
         }
 
-       else if (vt.voteProposalAttributes[_id].voteType == 222){
-        vt.voteProposalAttributes[_id].voteStatus = 222;
+        //withdraw
+       else if (vt.voteProposalAttributes[_id].voteType == 602){
+        vt.voteProposalAttributes[_id].voteStatus = 602;
         address addr = vt.voteProposalAttributes[_id].tokenID;
         _withdraw(addr, _amount);
         }
 
-        else if (vt.voteProposalAttributes[_id].voteType == 223){
-        vt.voteProposalAttributes[_id].voteStatus = 223;
+        //withdrawETH
+        else if (vt.voteProposalAttributes[_id].voteType == 603){
+        vt.voteProposalAttributes[_id].voteStatus = 603;
         uint withdrawAmount = _withdraw(wrappedNativeToken, _amount); 
         IWrappedNativeToken(wrappedNativeToken).withdraw(withdrawAmount);   
         }
-        
-        else {revert COULD_NOT_PROCESS("Not AaveV3 Type");}
+        else {revert COULD_NOT_PROCESS("Not AaveV2 Type");}
      
      emit VoteProposalLib.VoteStatus(
             _id,
@@ -78,9 +80,9 @@ contract AaveV3Facet is ERC2771ContextUpgradeable, HandlerBase {
         VoteProposalLib.checkForwarder(); 
     }
 
-     function executeAaveV3BorrowRepay(
+     function executeAaveV2BorrowRepay(
         uint24 _id,
-         uint256 rateMode
+        uint256 rateMode
     ) external {
        address msgSender_ = _msgSender();   
         VoteProposalLib.enforceMarried();
@@ -91,39 +93,41 @@ contract AaveV3Facet is ERC2771ContextUpgradeable, HandlerBase {
 
         uint256 _amount = vt.voteProposalAttributes[_id].amount;
 
-        
-        if (vt.voteProposalAttributes[_id].voteType == 224){
-        vt.voteProposalAttributes[_id].voteStatus = 224;
 
+        //borrow
+        if (vt.voteProposalAttributes[_id].voteType == 604){
+        vt.voteProposalAttributes[_id].voteStatus = 604;
           address onBehalfOf = address(this);
           address addr = vt.voteProposalAttributes[_id].tokenID;
             _borrow(addr, _amount, rateMode, onBehalfOf);
         }
+        //borrowETH
+         else if (vt.voteProposalAttributes[_id].voteType == 605){
+            vt.voteProposalAttributes[_id].voteStatus = 605;
 
-         else if (vt.voteProposalAttributes[_id].voteType == 225){
-            vt.voteProposalAttributes[_id].voteStatus =225;
-
-          address onBehalfOf = address(this);
+        address onBehalfOf = address(this);
          _borrow(wrappedNativeToken, _amount, rateMode, onBehalfOf);
          IWrappedNativeToken(wrappedNativeToken).withdraw(_amount);
         }
-
-         else if (vt.voteProposalAttributes[_id].voteType == 226){
-            vt.voteProposalAttributes[_id].voteStatus =226;
+        
+        //repay
+         else if (vt.voteProposalAttributes[_id].voteType == 606){
+            vt.voteProposalAttributes[_id].voteStatus =606;
 
           address onBehalfOf = address(this);
           address addr = vt.voteProposalAttributes[_id].tokenID;
            _repay(addr, _amount, rateMode, onBehalfOf);
         }
 
-          else if (vt.voteProposalAttributes[_id].voteType == 227){
-            vt.voteProposalAttributes[_id].voteStatus =227;
+        //repayETH
+          else if (vt.voteProposalAttributes[_id].voteType == 607){
+            vt.voteProposalAttributes[_id].voteStatus = 607;
             address onBehalfOf = address(this);
             IWrappedNativeToken(wrappedNativeToken).deposit{value: _amount}();
             _repay(wrappedNativeToken, _amount, rateMode, onBehalfOf);
         }
         
-        else {revert COULD_NOT_PROCESS("Not AaveV3 Borrow Type");}
+        else {revert COULD_NOT_PROCESS("Not AaveV2 Type");}
      
      emit VoteProposalLib.VoteStatus(
             _id,
@@ -131,35 +135,41 @@ contract AaveV3Facet is ERC2771ContextUpgradeable, HandlerBase {
             vt.voteProposalAttributes[_id].voteStatus,
             block.timestamp
         ); 
+
         VoteProposalLib.checkForwarder(); 
     }
 
+
       /* ========== INTERNAL FUNCTIONS ========== */
 
-    function _supply(address asset, uint256 amount) internal {
-        
-        (address pool, ) = _getPoolAndAToken(asset);
+      function _deposit(address asset, uint256 amount) internal {
+        (address pool, ) = _getLendingPoolAndAToken(asset);
         _tokenApprove(asset, pool, amount);
         try
-            IPool(pool).supply(asset, amount, address(this), REFERRAL_CODE)
+            ILendingPoolV2(pool).deposit(
+                asset,
+                amount,
+                address(this),
+                REFERRAL_CODE
+            )
         {} catch Error(string memory reason) {
             revert COULD_NOT_PROCESS(reason); 
         } catch {
-            revert COULD_NOT_PROCESS("supply");
+           revert COULD_NOT_PROCESS("deposit");
         }
         _tokenApproveZero(asset, pool);
     }
 
-     function _withdraw(address asset, uint256 amount)
+       function _withdraw(address asset, uint256 amount)
         internal
         returns (uint256 withdrawAmount)
     {
-        (address pool, address aToken) = _getPoolAndAToken(asset);
+        (address pool, address aToken) = _getLendingPoolAndAToken(asset);
         amount = _getBalance(aToken, amount);
 
-        try IPool(pool).withdraw(asset, amount, address(this)) returns (
-            uint256 ret
-        ) {
+        try
+            ILendingPoolV2(pool).withdraw(asset, amount, address(this))
+        returns (uint256 ret) {
             withdrawAmount = ret;
         } catch Error(string memory reason) {
             revert COULD_NOT_PROCESS(reason); 
@@ -174,10 +184,11 @@ contract AaveV3Facet is ERC2771ContextUpgradeable, HandlerBase {
         uint256 rateMode,
         address onBehalfOf
     ) internal {
-        address pool = IPoolAddressesProvider(PROVIDER).getPool();
+        address pool =
+            ILendingPoolAddressesProviderV2(provider).getLendingPool();
 
         try
-            IPool(pool).borrow(
+            ILendingPoolV2(pool).borrow(
                 asset,
                 amount,
                 rateMode,
@@ -185,56 +196,59 @@ contract AaveV3Facet is ERC2771ContextUpgradeable, HandlerBase {
                 onBehalfOf
             )
         {} catch Error(string memory reason) {
-            revert COULD_NOT_PROCESS(reason); 
+            revert COULD_NOT_PROCESS(reason);
         } catch {
             revert COULD_NOT_PROCESS("withdraw");
         }
     }
-    function _repay(
+
+     function _repay(
         address asset,
         uint256 amount,
         uint256 rateMode,
         address onBehalfOf
     ) internal returns (uint256 remainDebt) {
-        address pool = IPoolAddressesProvider(PROVIDER).getPool();
+        address pool =
+            ILendingPoolAddressesProviderV2(provider).getLendingPool();
         _tokenApprove(asset, pool, amount);
 
         try
-            IPool(pool).repay(asset, amount, rateMode, onBehalfOf)
+            ILendingPoolV2(pool).repay(asset, amount, rateMode, onBehalfOf)
         {} catch Error(string memory reason) {
-            revert COULD_NOT_PROCESS(reason); 
+            revert COULD_NOT_PROCESS(reason);
         } catch {
             revert COULD_NOT_PROCESS("repay");
         }
-
         _tokenApproveZero(asset, pool);
 
         DataTypes.ReserveData memory reserve =
-            IPool(pool).getReserveData(asset);
+            ILendingPoolV2(pool).getReserveData(asset);
         remainDebt = DataTypes.InterestRateMode(rateMode) ==
             DataTypes.InterestRateMode.STABLE
             ? IERC20(reserve.stableDebtTokenAddress).balanceOf(onBehalfOf)
             : IERC20(reserve.variableDebtTokenAddress).balanceOf(onBehalfOf);
     }
 
-    function _getPoolAndAToken(address underlying)
+
+     function _getLendingPoolAndAToken(address underlying)
         internal
         view
         returns (address pool, address aToken)
     {
-        pool = IPoolAddressesProvider(PROVIDER).getPool();
-        try IPool(pool).getReserveData(underlying) returns (
+        pool = ILendingPoolAddressesProviderV2(provider).getLendingPool();
+        try ILendingPoolV2(pool).getReserveData(underlying) returns (
             DataTypes.ReserveData memory data
         ) {
             aToken = data.aTokenAddress;
-            if (aToken == address(0)) {
+             if (aToken == address(0)) {
                 revert COULD_NOT_PROCESS("aToken should not be zero"); 
             }
         } catch Error(string memory reason) {
-                revert COULD_NOT_PROCESS(reason); 
+            revert COULD_NOT_PROCESS(reason); 
         } catch {
             revert COULD_NOT_PROCESS("General");
         }
     }
+
 }
 
