a2719f3ea10e679535bdaf86c5e08d36a1698b52
Ismailov Altynbek <altyni@gmail.com>
2023-03-19 14:21:58 +0600

    Added goerli deployment script

--- scripts/deployGoerli.js
+++ scripts/deployGoerli.js
@@ -16,35 +16,24 @@ function sleep(milliseconds) {
 
 async function main() {
   console.log("Construction started.....");
-  const net = 5;
-  baseAssetAddress = networks[net].USDC;
-  usdcAddress = baseAssetAddress;
+  const net =  "usdc-goerli";
   cometAddress = networks[net].comet;
   wethAddress = networks[net].WETH;
-  rewardsCOMP = networks[net].rewards;
   CompAddress = networks[net].comptroller;
   daiAddress = networks[net].DAI;
-  usdtAddress = networks[net].USDT;
   MakerProxyRegistryAddress = networks[net].MakerProxyRegistry;
   MakerChainLogAddress = networks[net].MakerChainLog;
   BPCDPManagerAddress = networks[net].BPCDPManager;
   BPProxyActionsAddress = networks[net].BPProxyActions;
-  MakerDaiAddress = networks[net].MAKER_MCD_JOIN_DAI;
-  MakerETHAddress = networks[net].MAKER_MCD_JOIN_ETH_A;
-  MakerVATAddress = networks[net].MAKER_MCD_VAT;
-  MakerUSDCAddress = networks[net].MAKER_MCD_JOIN_USDC_A;
+
   UniswapRouterAddress = networks[net].uniswapRouter;
   UniswapRouterV2Address = networks[net].uniswapV2Router;
   SushiSwapRouterAddress = networks[net].SushiSwapRouter;
   QuickSwapRouterAddress = networks[net].QuickSwapRouter;
   AaveV2ProviderAddress = networks[net].aaveV2Provider;
   AaveV3ProviderAddress = networks[net].aaveV3Provider;
-  CurveMinterAddress = networks[net].curveMinter;
-  CRVTokenAddress = networks[net].CRV_TOKEN;
   MakerCDPManagerAddress = networks[net].MakerCDPManager;
   MakerProxyActionsAddress = networks[net].MakerProxyActions;
-  AugustusAddress = networks[net].AUGUSTUS_SWAPPER;
-  TokenProxy = networks[net].TOKEN_TRANSFER_PROXY;
 
   const forwarder = await deploy("MinimalForwarder");
 
@@ -97,6 +86,12 @@ async function main() {
     Status: 1,
   });
 
+  console.log(
+    "CompoundV2Facet deployed:",
+    CompoundV2Facet.address,
+    CompoundV2Facet.deployTransaction.gasLimit
+  );
+
   //3. Uniswap V3
   UniSwapV3Facet = await deploy(
     "UniSwapV3Facet",
@@ -109,6 +104,12 @@ async function main() {
     ContractAddress: UniSwapV3Facet.address,
     Status: 1,
   });
+
+  console.log(
+    "UniSwapV3Facet deployed:",
+    UniSwapV3Facet.address,
+    UniSwapV3Facet.deployTransaction.gasLimit
+  );
   //4. Uniswap V2
   UniSwapV2Facet = await deploy(
     "UniSwapV2Facet",
@@ -121,6 +122,12 @@ async function main() {
     Status: 1,
   });
 
+  console.log(
+    "UniSwapV2Facet deployed:",
+    UniSwapV2Facet.address,
+    UniSwapV2Facet.deployTransaction.gasLimit
+  );
+
   //5. SushiSwap
 
   SushiSwapFacet = await deploy(
@@ -132,6 +139,12 @@ async function main() {
     ContractAddress: SushiSwapFacet.address,
     Status: 1,
   });
+
+  console.log(
+    "SushiSwapFacet deployed:",
+    SushiSwapFacet.address,
+    SushiSwapFacet.deployTransaction.gasLimit
+  );
 
   //6. QuickSwap
 
@@ -146,6 +159,13 @@ async function main() {
     Status: 1,
   });
 
+
+  console.log(
+    "QuickSwapV2Facet deployed:",
+    QuickSwapV2Facet.address,
+    QuickSwapV2Facet.deployTransaction.gasLimit
+  );
+
   //7. Aave V2
   AaveV2Facet = await deploy(
     "AaveV2Facet",
@@ -158,6 +178,13 @@ async function main() {
     ContractAddress: AaveV2Facet.address,
     Status: 1,
   });
+
+  console.log(
+    "AaveV2Facet deployed:",
+    AaveV2Facet.address,
+    AaveV2Facet.deployTransaction.gasLimit
+  );
+
 
   //8.AaveV3
   AaveV3Facet = await deploy(
@@ -172,6 +199,13 @@ async function main() {
     Status: 1,
   });
 
+   console.log(
+    "AaveV3Facet deployed:",
+    AaveV3Facet.address,
+    AaveV3Facet.deployTransaction.gasLimit
+  );
+
+
   //9.Yearn
 
   YearnFacet = await deploy("YearnFacet", forwarder.address);
@@ -181,17 +215,23 @@ async function main() {
     Status: 1,
   });
 
-  //10.Curve
-  CurveFacet = await deploy(
-    "CurveFacet",
-    forwarder.address,
-    CurveMinterAddress,
-    CRVTokenAddress
+  console.log(
+    "YearnFacet deployed:",
+    YearnFacet.address,
+    YearnFacet.deployTransaction.gasLimit
   );
-  WhiteListAddr.push({
-    ContractAddress: CurveFacet.address,
-    Status: 1,
-  });
+
+  // //10.Curve ==> Not available in GOerli
+  // CurveFacet = await deploy(
+  //   "CurveFacet",
+  //   forwarder.address,
+  //   CurveMinterAddress,
+  //   CRVTokenAddress
+  // );
+  // WhiteListAddr.push({
+  //   ContractAddress: CurveFacet.address,
+  //   Status: 1,
+  // });
 
   //11.Maker
   MakerFacet = await deploy(
@@ -210,52 +250,39 @@ async function main() {
     Status: 1,
   });
 
-  //12.Paraswap
-
-  ParaSwapFacet = await deploy(
-    "ParaSwapFacet",
-    forwarder.address,
-    AugustusAddress,
-    TokenProxy
-  );
-  WhiteListAddr.push({
-    ContractAddress: ParaSwapFacet.address,
-    Status: 1,
-  });
-
-  //13.OneInch
-
-  OneInchV5Facet = await deploy(
-    "OneInchV5Facet",
-    forwarder.address,
-    TokenProxy
-  );
-
-  WhiteListAddr.push({
-    ContractAddress: OneInchV5Facet.address,
-    Status: 1,
-  });
-
-  const UniSwapFacet = await deploy(
-    "UniSwapFacet",
-    "0xE592427A0AEce92De3Edee1F18E0157C05861564",
-    "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
-    forwarder.address
-  );
-  //0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 --> Mainnet weth9 address
-  //0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6 -->Goerli weth9 address
-  //0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270 --> Polygon WMATIC
   console.log(
-    "UniSwapFacet deployed:",
-    UniSwapFacet.address,
-    UniSwapFacet.deployTransaction.gasLimit
+    "MakerFacet deployed:",
+    MakerFacet.address,
+    MakerFacet.deployTransaction.gasLimit
   );
-  WhiteListAddr.push({
-    ContractAddress: UniSwapFacet.address,
-    Status: 1,
-  });
 
-  const familyDao = await deploy("FamilyDAOFacet", forwarder.address);
+  //12.Paraswap -> No Goerli
+
+  // ParaSwapFacet = await deploy(
+  //   "ParaSwapFacet",
+  //   forwarder.address,
+  //   AugustusAddress,
+  //   TokenProxy
+  // );
+  // WhiteListAddr.push({
+  //   ContractAddress: ParaSwapFacet.address,
+  //   Status: 1,
+  // });
+
+  //13.OneInch --> No Goerli
+
+  // OneInchV5Facet = await deploy(
+  //   "OneInchV5Facet",
+  //   forwarder.address,
+  //   TokenProxy
+  // );
+
+  // WhiteListAddr.push({
+  //   ContractAddress: OneInchV5Facet.address,
+  //   Status: 1,
+  // });
+
+const familyDao = await deploy("FamilyDAOFacet", forwarder.address);
   console.log(
     "FamilyDAO contract deployed:",
     familyDao.address,
@@ -335,12 +362,21 @@ async function main() {
   );
 
   writeFileSync(
-    "deploytest-goerli-latest.json",
+    "deploytest-Goerli-latestV2.json",
     JSON.stringify(
       {
         DiamondCutFacet: DiamondCutFacet.address,
-        CompoundFacet: CompoundFacet.address,
-        UniSwapFacet: UniSwapFacet.address,
+        CompoundV3FacetUSDC: CompoundV3FacetUSDC.address,
+        CompoundV2Facet: CompoundV2Facet.address,
+        UniSwapV3Facet: UniSwapV3Facet.address,
+        UniSwapV2Facet: UniSwapV2Facet.address,
+        SushiSwapFacet: SushiSwapFacet.address,
+        QuickSwapV2Facet: QuickSwapV2Facet.address,
+        AaveV2Facet: AaveV2Facet.address,
+        AaveV3Facet: AaveV3Facet.address,
+        YearnFacet: YearnFacet.address,
+        MakerFacet: MakerFacet.address,
+        MakerInit2: MakerInit2.address, 
         familyDao: familyDao.address,
         nftViewContract: nftViewContract.address,
         nftContract: nftContract.address,
@@ -359,67 +395,67 @@ async function main() {
   var txn;
   txn = await nftViewContract.changenftmainAddress(nftContract.address);
   console.log("NFT Main Address updated");
-  txn.wait();
+  txn.wait(1);
 
   txn = await nftViewContract.changeMainAddress(WavePortal7.address);
   console.log("NFT Address updated in View");
-  txn.wait();
+  txn.wait(1);
 
   txn = await nftContract.changeMainAddress(WavePortal7.address);
   console.log("NFT Address updated");
-  txn.wait();
+  txn.wait(1);
 
   txn = await WaverFactory.changeAddress(WavePortal7.address);
   console.log("Wavefactory Main Address updated");
-  txn.wait();
+  txn.wait(1);
 
   txn = await WavePortal7.changeaddressNFT(
     nftContract.address,
     nftSplit.address
   );
   console.log("WavePortal Split Address updated");
-  txn.wait();
+  txn.wait(1);
 
   console.log("WhiteList:", WhiteListAddr);
 
   txn = await WavePortal7.whiteListAddr(WhiteListAddr);
   console.log("WavePortal Whitelist Addresses added");
-  txn.wait();
+  txn.wait(2);
 
   txn = await nftViewContract.addheartPatterns(
     0,
     "0x3c6c696e6561724772616469656e742069643d227022203e3c73746f70206f66667365743d22302522207374796c653d2273746f702d636f6c6f723a20233930363b2073746f702d6f7061636974793a2030222f3e3c2f6c696e6561724772616469656e743e"
   );
   console.log("Updated patterns");
-  txn.wait();
+  txn.wait(2);
 
   txn = await nftViewContract.addadditionalGraphics(
     0,
     "0x3c6c696e6561724772616469656e742069643d227022203e3c73746f70206f66667365743d22302522207374796c653d2273746f702d636f6c6f723a20233930363b2073746f702d6f7061636974793a2030222f3e3c2f6c696e6561724772616469656e743e"
   );
   console.log("Updated Graphics");
-  txn.wait();
+  txn.wait(2);
 
   txn = await nftViewContract.addcertBackground(
     0,
     "0x3c6c696e6561724772616469656e742069643d274227206772616469656e74556e6974733d277573657253706163654f6e557365272078313d272d392e393525272079313d2733302e333225272078323d273130392e393525272079323d2736392e363825273e3c73746f70206f66667365743d272e343438272073746f702d636f6c6f723d2723433537424646272f3e3c73746f70206f66667365743d2731272073746f702d636f6c6f723d2723333035444646272f3e3c2f6c696e6561724772616469656e743e"
   );
   console.log("Added Background");
-  txn.wait();
+  txn.wait(2);
 
   txn = await nftViewContract.addcertBackground(
     1001,
     "0x3c6c696e6561724772616469656e742069643d2242222078313d22353025222079313d223025222078323d22353025222079323d2231303025223e3c73746f70206f66667365743d223025222073746f702d636f6c6f723d2223374135464646223e3c616e696d617465206174747269627574654e616d653d2273746f702d636f6c6f72222076616c7565733d22233741354646463b20233031464638393b202337413546464622206475723d2234732220726570656174436f756e743d22696e646566696e697465222f3e3c2f73746f703e3c73746f70206f66667365743d2231303025222073746f702d636f6c6f723d2223303146463839223e3c616e696d617465206174747269627574654e616d653d2273746f702d636f6c6f72222076616c7565733d22233031464638393b20233741354646463b202330314646383922206475723d2234732220726570656174436f756e743d22696e646566696e697465222f3e3c2f73746f703e3c2f6c696e6561724772616469656e743e"
   );
   console.log("Added Background 2");
-  txn.wait();
+  txn.wait(2);
 
   txn = await nftViewContract.addheartPatterns(
     101,
     "0x3c6c696e6561724772616469656e742069643d2270222078313d2230222078323d22313131222079313d223330222079323d22323022206772616469656e74556e6974733d227573657253706163654f6e557365223e3c73746f702073746f702d636f6c6f723d222346463542393922206f66667365743d22313025222f3e3c73746f702073746f702d636f6c6f723d222346463534343722206f66667365743d22323025222f3e3c73746f702073746f702d636f6c6f723d222346463742323122206f66667365743d22343025222f3e3c73746f702073746f702d636f6c6f723d222345414643333722206f66667365743d22363025222f3e3c73746f702073746f702d636f6c6f723d222334464342364222206f66667365743d22383025222f3e3c73746f702073746f702d636f6c6f723d222335314637464522206f66667365743d2231303025222f3e3c2f6c696e6561724772616469656e743e"
   );
   console.log("Added Patterns 2");
-  txn.wait();
+  txn.wait(2);
 
   console.log("Construction completed!");
 }
